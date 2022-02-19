fc1df92217f7d122aff7a9a5f84a03a6b6613521
acmol <iamacmol+github@gmail.com>
2022-02-19 12:14:09 +0800

    add uncutted trait

--- Plugin.cs
+++ Plugin.cs
@@ -17,7 +17,7 @@ namespace HarderKCM
     {
         private const string PluginGuid = "acmol.kcm.endless";
         private const string PluginName = "Kaycee's Endless Mode";
-        private const string PluginVersion = "0.0.4";
+        private const string PluginVersion = "0.0.5";
 
         private void Awake()
         {
@@ -34,9 +34,9 @@ namespace HarderKCM
             Harmony harmony = new Harmony(PluginGuid);
             harmony.PatchAll();
             Harder();
+            
         }
         static public ManualLogSource Log = null;
-
         static public void set_card_info(CardInfo card, int base_attack, int base_health) {
             var baseTraverse = Traverse.Create(card);
 				    baseTraverse.Field("baseAttack").SetValue(base_attack);
@@ -60,7 +60,6 @@ namespace HarderKCM
         // Every card will be called exactly once.
         public static event PlayableCardCallback OnCardEmergeOnBoardEvent;
 
-        public static int MaxDifficulty = 100;
         public static int RepeatLevel = 3;
         private void Harder() {
             // Grizzly is stronger now
@@ -95,25 +94,79 @@ namespace HarderKCM
     }
 
     class GrizzlyModifier {
+        public static void OnGrizzlyGlitchStart(int level) {
+            _glitching = true;
+            noCutting = true;
+            _level = level;
+            currentModifies = new List<CardModify> {GrizzlyIncreaseStat};
+            var mods = new List<CardModify>{GrizzlyAddStone, StarvationAddReach, AddNoCutting};;
+            int n = level + 1;
+            if (n >= 4 && level <= 5) {
+                currentModifies.Add(GrizzlyAndStarvationAddDeathTouch);
+            } 
+            if (n >= 6 && level <= 8) {
+                var mod = mods[UnityEngine.Random.Range(0, mods.Count())];
+                if (mod == AddNoCutting) {
+                    noCutting = true;
+                }
+                currentModifies.Add(mod);
+            } 
+            if (n == 9) {
+                currentModifies.AddRange(mods);
+                noCutting = true;
+            }
+        }
+        private static int _level;
+        private static bool _glitching;
 
-        static public void starvation_add_reach(PlayableCard card) { 
+        private static List<CardModify> currentModifies = null;
+
+        public static void OnGrizzlyGlitchEnd() {
+            noCutting = false;
+            _glitching = false;
+        }
+
+        static public void GrizzlyIncreaseStat(PlayableCard card) { 
+            if (card.Info.name == "Grizzly") {
+                API.Log.LogInfo($"modify Grizzly");
+                var mod = new CardModificationInfo(_level * 4, _level * 6);
+                card.AddTemporaryMod(mod);
+            }
+        }
+        static public void StarvationAddReach(PlayableCard card) { 
             if (card.Info.name == "Starvation") {
                 card.AddTemporaryMod(new CardModificationInfo(Ability.Reach));
             }
         }
 
-        static public void grizzly_add_stone(PlayableCard card) { 
+        static public void GrizzlyAddStone(PlayableCard card) { 
             if (card.Info.name == "Grizzly") {
                 card.AddTemporaryMod(new CardModificationInfo(Ability.MadeOfStone));
             }
         }
 
-        // static public void grizzly_add_uncutable(PlayableCard card) {
-        //     if (card.Info.name == "Grizzly") {
-        //         card.Info.traits.Add(Trait.Uncuttable);
-        //     }
-        // }
+        static public void GrizzlyAndStarvationAddDeathTouch(PlayableCard card) { 
+            if (card.Info.name == "Grizzly" || card.Info.name == "Starvation") {
+                card.AddTemporaryMod(new CardModificationInfo(Ability.Deathtouch));
+            }
+        }
 
+        static public void AddNoCutting(PlayableCard card) {
+        }
+
+        public static bool noCutting = false;
+        
+        public static IEnumerator AdditionalGlitchSequence() {
+            string tip = "No Cutting This Turn!";
+            if (Localization.CurrentLanguage == Language.ChineseSimplified) {
+                tip = "增加点难度，本局别想剪牌!";
+            }
+            if (noCutting) {
+                return Singleton<TextDisplayer>.Instance.ShowUntilInput($"[c:bR]{tip}[c:]", -2.5f, 0.5f, Emotion.Anger, TextDisplayer.LetterAnimation.Jitter, DialogueEvent.Speaker.Single, null, true);
+            } else {
+                return null;
+            }
+        }
         public delegate void CardModify(PlayableCard card);
 
         static public void Modify(ref PlayableCard card) {
@@ -127,37 +180,15 @@ namespace HarderKCM
             }            
             
             var current_node = RunState.Run.map.nodeData.Find((NodeData x) => x.id == RunState.Run.currentNodeId);
-            var mod_func = new List<CardModify> {grizzly_add_stone, starvation_add_reach};
 
             if (current_node is BossBattleNodeData) {
                 // Boss Level
-                if (card.OpponentCard) {
-                    if (info.name == "Grizzly") {
-                        API.Log.LogInfo($"modify Grizzly");
-                        var mod = new CardModificationInfo(level * 4, level * 6);
-                        card.AddTemporaryMod(mod);
-                    }
-                    if (level >= 3 && level <= 4) {
-                        if (info.name == "Starvation" || info.name == "Grizzly") {
-                            card.AddTemporaryMod(new CardModificationInfo(Ability.Deathtouch));
-                        }
-                    }
-
-                    if (level >= 5 && level <= 7) {
-                        var mod = mod_func[UnityEngine.Random.Range(0, mod_func.Count())];
+                if (card.OpponentCard && _glitching) {
+                    foreach (var mod in currentModifies) {
                         mod(card);
                     }
-
-                    if (level == 8) {
-                        foreach (var mod in mod_func) {
-                            mod(card);
-                        }
-                    }
                 }
-            } else {
-                var type = current_node.GetType();
-                API.Log.LogInfo($"{type}");
-            }
+            } 
             API.Log.LogInfo($"MODIFY END");
         }
     }
@@ -202,4 +233,51 @@ namespace HarderKCM
 
     }
 
+    [HarmonyPatch(typeof(ScissorsItem), "GetValidTargets")]
+    class ScissorsItem_GetValidTargets {
+        public static void Postfix(ref List<CardSlot>  __result) {
+            if (GrizzlyModifier.noCutting) {
+                __result = new List<CardSlot>();
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Part1BossOpponent), "GrizzlyGlitchSequence")]
+    class Part1BossOpponent_GrizzlyGlitchSequence {
+
+        public static bool isGrizzlyGlitching = false;
+        public static IEnumerator getReplacedSequence(IEnumerator result) {
+            while (result.MoveNext()) {
+                yield return result.Current;
+            }
+            var additional_sequence = GrizzlyModifier.AdditionalGlitchSequence();
+            if (additional_sequence != null) {
+                yield return additional_sequence;
+            }
+            yield break;
+        }
+
+        public static void Prefix() {
+            API.Log.LogInfo("Grizzly Glitching!!!");
+            isGrizzlyGlitching = true;
+            int level = RunState.CurrentRegionTier;
+            GrizzlyModifier.OnGrizzlyGlitchStart(level);
+        }
+        public static void Postfix(ref IEnumerator __result) {
+            API.Log.LogInfo("Grizzly Glitching Postfix!!!");
+            var old_result = __result;
+            __result = getReplacedSequence(old_result);
+        }
+    }
+
+    [HarmonyPatch(typeof(Part1BossOpponent), "BossDefeatedSequence")]
+    class Part1BossOpponent_BossDefeatedSequence {
+        public static void Postfix() {
+            API.Log.LogInfo("BossDefeatedSequence!!");
+            if (Part1BossOpponent_GrizzlyGlitchSequence.isGrizzlyGlitching) {
+                GrizzlyModifier.OnGrizzlyGlitchEnd();
+                Part1BossOpponent_GrizzlyGlitchSequence.isGrizzlyGlitching = false;
+            }
+        }
+    }
 }
